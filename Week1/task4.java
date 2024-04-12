ea14c47371de5e0ebc63dfe225e5e7fa73ee100c
Deepa131 <deepapaudel65@gmail.com>
2024-04-11 22:40:58 -0700

    Week1 task3

---
+++ Week1/task4.java
@@ -0,0 +1,19 @@
+package Task.Week1;
+
+public class task4 {
+    public static void main(String[] args) {
+//         4. WAP that prints the following pattern:
+
+// *
+
+// **
+
+// ***
+
+// ****
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
